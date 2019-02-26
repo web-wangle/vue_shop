0aa45b26031fff6ab79b5e6a70d2f586613c06aa
web-wangle <18829291736@163.com>
2019-02-26 15:31:55 +0800

    主页图标组件开发

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
           </div>
           <router-link to="/sign" class="head_right">登录</router-link>
         </header>
-        <section id="swiper">
+        <section class="swiper">
           <van-swipe :autoplay="3000" indicator-color="white">
             <van-swipe-item v-for="(item, index) in swiperArray" :key="index">
               <a :href="item.swiperUrl">
@@ -23,7 +23,14 @@
             </van-swipe-item>
           </van-swipe>
         </section>
-        <section>此处为icon合集</section>
+        <section class="nav">
+          <div>
+            <a :href="item.navUrl" v-for="(item, index) in navArray" :key="index" :class="'navIcon-'+index">
+              <div class="navIcon"></div>
+              <p class="nevTxt">{{item.navTxt}}</p>
+            </a>
+          </div>
+        </section>
         <section>此处为攻略</section>
         <footer>此处为footer</footer>
       </ul>
@@ -41,7 +48,7 @@ export default {
       if (!this.scroll) {
         this.scroll = new BScroll(this.$refs.wrapper, {
           scrollY: true,
-          click: true
+          eventPassthrough: 'horizontal'
         })
       } else {
         this.scroll.refresh()
@@ -62,6 +69,40 @@ export default {
         {
           swiperUrl: 'http://m.mafengwo.cn/i/11828398.html',
           swiperImage: 'https://n1-q.mafengwo.net/s13/M00/C9/F4/wKgEaVxsB0uATeQnAARcFCA00Rc14.jpeg?imageMogr2%2Fthumbnail%2F%21750x422r%2Fgravity%2FCenter%2Fcrop%2F%21750x422%2Fquality%2F90'
+        }
+      ],
+      'navArray': [
+        {
+          navTxt: '找攻略',
+          navUrl: 'https://m.mafengwo.cn/mdd/'
+        },
+        {
+          navTxt: '看游记',
+          navUrl: 'https://m.mafengwo.cn/note/'
+        },
+        {
+          navTxt: '问达人',
+          navUrl: 'https://m.mafengwo.cn/wenda/'
+        },
+        {
+          navTxt: '结伴',
+          navUrl: 'https://m.mafengwo.cn/together/'
+        },
+        {
+          navTxt: '酒店',
+          navUrl: 'https://m.mafengwo.cn/hotel/#/home'
+        },
+        {
+          navTxt: '旅行商城',
+          navUrl: 'https://m.mafengwo.cn/sales/'
+        },
+        {
+          navTxt: '机票',
+          navUrl: 'https://m.mafengwo.cn/flight/'
+        },
+        {
+          navTxt: '当地玩乐',
+          navUrl: 'https://m.mafengwo.cn/localdeals/'
         }
       ]
     }
@@ -136,12 +177,94 @@ header{
     color: #ffa800;
   }
 }
-#swiper{
+.swiper{
   width: 100%;
   height: 2.1rem;
   img{
     width: 100%;
     height: 2.1rem;
+  }
+}
+.nav{
+  div{
+    width: auto;
+    height: 2.16rem;
+    margin-top: 10px;
+    padding: 0 0.05rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    a{
+      padding-top: 0.2rem;
+      width: 0.9rem;
+      height: 0.92rem;
+      box-sizing: border-box;
+      font-size: 0.16rem;
+      .navIcon{
+        margin: 0 auto 0.06rem auto;
+        padding: 0;
+        width: 0.46rem;
+        height: 0.46rem;
+        background-image: url('../assets/images/home/i_nav3.png');
+        background-size: 1.9rem;
+      }
+      p{
+        width: inherit;
+        display: table-cell;
+        height: 0.2rem;
+        text-align: center;
+        vertical-align: bottom;
+      }
+    }
+    .navIcon-0{
+      color: #ff9d00;
+      .navIcon{
+        background-position: 0 0;
+      }
+    }
+    .navIcon-1{
+      color: #feca2b;
+      .navIcon{
+        background-position: 0 0.47rem;
+      }
+    }
+    .navIcon-2{
+      color: #42d6ba;
+      .navIcon{
+        background-position: 0.96rem 0.47rem;
+      }
+    }
+    .navIcon-3{
+      color: #f94a87;
+      .navIcon{
+        background-position: 1.43rem 0.47rem;
+      }
+    }
+    .navIcon-4{
+      color: #32a2f1;
+      .navIcon{
+        background-position: 1.43rem 0;
+      }
+    }
+    .navIcon-5{
+      color: #fd4e4e;
+      .navIcon{
+        background-position: 0.96rem 0;
+      }
+    }
+    .navIcon-6{
+      color: #acce0e;
+      .navIcon{
+        background-position: 0.49rem 0;
+      }
+    }
+    .navIcon-7{
+      color: #b160df;
+      .navIcon{
+        background-position: 0.49rem 0.47rem;
+      }
+    }
   }
 }
 </style>
